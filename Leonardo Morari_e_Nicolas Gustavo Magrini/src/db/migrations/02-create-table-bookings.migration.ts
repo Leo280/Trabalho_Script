fcfa98503db1617f302e72b4c4f73493f545499c
Nicolas Gustavo Magrini Pereira <nicolas.magrini@compasso.com.br>
2024-12-15 11:27:06 -0300

    models and migrations

---
+++ Leonardo Morari_e_Nicolas Gustavo Magrini/src/db/migrations/02-create-table-bookings.migration.ts
@@ -0,0 +1,17 @@
+import { Kysely } from 'kysely';
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('bookings')
+    .ifNotExists()
+    .addColumn('id', 'text', (col) => col.primaryKey())
+    .addColumn('date', 'text', (col) => col.notNull())
+    .addColumn('cpf', 'text', (col) => col.notNull())
+    .addColumn('time', 'text', (col) => col.notNull())
+    .addColumn('user_id', 'text', (col) => col.references('users.id').onDelete('cascade').notNull())
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropTable('reservations').ifExists().execute();
+}
